8b253cb82edfd0ccbf2ccc8e2e2432f1be25a46d
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-04-05 12:35:07 +0200

    Hemos implementado metodos adicionales; solicitarNumero, solicitarOperacion, realizarOperacion

--- M05UF2Practica1/src/practica1/Practica1.java
+++ M05UF2Practica1/src/practica1/Practica1.java
@@ -9,80 +9,77 @@ import java.util.Scanner;
 
 public class Practica1 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double res = 0;
+    // Método para solicitar un número al usuario
+    private static double solicitarNumero(Scanner sc, String mensaje) {
+        String input;
+        do {
+            System.out.println(mensaje);
+            input = sc.nextLine();
+        } while (!input.matches("[+-]?[\\d]*[.]?[\\d]+")); // Comprueba si es un número válido
+        return Double.parseDouble(input);
+    }
+
+    // Método para solicitar una operación válida al usuario
+    private static String solicitarOperacion(Scanner sc) {
         String operacion;
         boolean comprobar;
+        do {
+            System.out.println("\n Operación? (Indica el signo)");
+            System.out.println("+ = sumar \n - = restar \n"
+                    + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
+                    + "\n % = residuo");
+            operacion = sc.nextLine();
+            // Comprueba si la operación es válida
+            if (operacion.matches("[+\\-xX/*%]")) {
+                comprobar = true;
+            } else {
+                comprobar = false;
+            }
+        } while (!comprobar);
+        return operacion;
+    }
+
+    // Método para realizar una operación aritmética
+    private static double realizarOperacion(double nume1, double nume2, String operacion) {
+        double resultado = 0;
+        switch (operacion) {
+            case "+":
+                resultado = nume1 + nume2;
+                break;
+            case "-":
+                resultado = nume1 - nume2;
+                break;
+            case "x":
+            case "X":
+                resultado = nume1 * nume2;
+                break;
+            case "/":
+                resultado = nume1 / nume2;
+                break;
+            case "*":
+                resultado = Math.pow(nume1, nume2);
+                break;
+            case "%":
+                resultado = nume1 % nume2;
+                break;
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double resultado = 0;
+        String operacion;
 
         do {
-            // Pedir el primer número
-            String numero1;
-            do {
-                System.out.println("\n Introduce el primer número: ");
-                numero1 = sc.nextLine();
-            } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume1 = Double.parseDouble(numero1);
-
-            // Pedir la operación
-            do {
-                System.out.println("\n Operación? (Indica el signo)");
-                System.out.println("+ = sumar \n - = restar \n"
-                        + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
-                        + "\n % = residuo");
-                operacion = sc.nextLine();
-                // Comprueba si la operación es válida
-                if (operacion.matches("[+\\-xX/*%]")) {
-                    comprobar = true;
-                } else {
-                    comprobar = false;
-                }
-            } while (!comprobar);
-
-            // Pedir el segundo número
-            String numero2;
-            do {
-                System.out.println("\n Introduce el segundo número: ");
-                numero2 = sc.nextLine();
-            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume2 = Double.parseDouble(numero2);
-
-            // Realizar la operación seleccionada
-            switch (operacion) {
-                case "+":
-                    res = nume1 + nume2;
-                    break;
-                case "-":
-                    res = nume1 - nume2;
-                    break;
-                case "x":
-                case "X":
-                    res = nume1 * nume2;
-                    break;
-                case "/":
-                    while (nume2 == 0) {
-                        System.err.println("El denominador no puede ser cero. Introduce otro valor: ");
-                        numero2 = sc.nextLine();
-                        nume2 = Double.parseDouble(numero2);
-                    }
-                    res = nume1 / nume2;
-                    break;
-                case "*":
-                    res = Math.pow(nume1, nume2);
-                    break;
-                case "%":
-                    while (nume2 == 0) {
-                        System.err.println("El denominador no puede ser cero. Introduce otro valor: ");
-                        numero2 = sc.nextLine();
-                        nume2 = Double.parseDouble(numero2);
-                    }
-                    res = nume1 % nume2;
-                    break;
-            }
+            double nume1 = solicitarNumero(sc, "\n Introduce el primer número: ");
+            String operacionSeleccionada = solicitarOperacion(sc);
+            double nume2 = solicitarNumero(sc, "\n Introduce el segundo número: ");
+            resultado = realizarOperacion(nume1, nume2, operacionSeleccionada);
 
             // Mostrar el resultado en formato de calculadora
             System.out.println("\n*******************************");
-            System.out.println("Resultado: " + nume1 + " " + operacion + " " + nume2 + " = (" + res + ")");
+            System.out.println("Resultado: " + nume1 + " " + operacionSeleccionada + " " + nume2 + " = (" + resultado + ")");
             System.out.println("*******************************");
 
             // Preguntar si desea continuar operando
